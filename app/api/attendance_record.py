49fafde7cc3aafdac28711b5fed234812a0a6fea
hu <2367156855@qq.com>
2025-12-05 17:31:48 +0800

    新增attendance_record.py查询考勤记录功能

--- app/api/attendance_record.py
+++ app/api/attendance_record.py
@@ -22,9 +22,9 @@ def attend_cod_add():
     work_hours:str=None 工时,
     location:str=None 打卡地点,
     approver_user_name:str=None 审批人,
-    approver_at:str=None 审批时间,
+    approved_at:str=None 审批时间,
     late_minutes:int = None 迟到分钟, 
-    early_leave_minute:int=None 早退分钟,
+    early_leave_minutes:int=None 早退分钟,
     remark:str=None 备注
     """
     if not request.is_json:
@@ -72,10 +72,10 @@ def attend_cod_update():
     checkout_time:str=None 下班打卡,
     work_hours:str=None 工时,
     location:str=None 打卡地点,
-    approver_user_name:str=None 审批人,
+    approved_user_name:str=None 审批人,
     approver_at:str=None 审批时间,
     late_minutes:int = None 迟到分钟, 
-    early_leave_minute:int=None 早退分钟,
+    early_leave_minutes:int=None 早退分钟,
     remark:str=None 备注,
     status:str=None enum('initial','corrected','approved','locked')
     """
@@ -105,5 +105,47 @@ def attend_cod_update():
         return response
     except Exception as e:
         return jsonify({"regulate_code":0,"column_name": ["error1142"],"data": [[str(e)]]})
+    finally:
+        conn.close()
+
+@api_bp.route('/attend_cod/select', methods=['POST'])
+def attend_cod_select():
+    """
+    username :str 用户,
+    passward :str 密码,
+    employee_no:str=None 员工编号,
+    date:st=None 日期,
+    absent:int = None 旷工标识,
+    shift_type:str = None 班次类型,
+    checkin_time:str=None 上班打卡,
+    source:str=None 数据来源 enum('device','import'),
+    checkout_time:str=None 下班打卡,
+    work_hours:str=None 工时,
+    location:str=None 打卡地点,
+    approver_user_name:str=None 审批人,
+    approved_at:str=None 审批时间,
+    late_minutes:int = None 迟到分钟, 
+    early_leave_minutes:int=None 早退分钟,
+    remark:str=None 备注
+    status:str=None enum('initial','corrected','approved','locked')
+    """
+    if not request.is_json:
+        return jsonify({"error":"Invalid type of post1113"})
+    try:
+        data = request.get_json()
+        conn = cm.connect_mysql(*cm.default)
+        status = lm.login_mysql(conn,data['username'],data['password'])
+        regulate_code = 0
+        response = None
+        if status:
+            regulate_code = lm.get_regulate_code(conn,data['username'])
+            response = am.select_attend_cod(conn,**data,r_flag = regulate_code)
+        else:
+            response = {"column_name": ["error"],"data": [["Unable to verify login1123"]]}
+            response["regulate_code"] = regulate_code
+        response["status"] = status
+        return response
+    except Exception as e:
+        return jsonify({"regulate_code":0,"column_name": ["error"],"data": [[str(e)],["attend_cod_select11133"]]})
     finally:
         conn.close()
--- app/services/attendance_record_mysql.py
+++ app/services/attendance_record_mysql.py
@@ -5,7 +5,7 @@ from app.utils import easy_query as eq
 from app.services import config_mysql as cf
 
 @regulate(0b1101)
-def add_attend_cod(conn:pymysql.Connection ,date: str,employee_no:str,absent:int = None,shift_type:str = None,checkin_time:str=None,source:str=None,checkout_time:str=None,work_hours:str=None,location:str=None,approver_user_name:str=None,approver_at:str=None,late_minutes:int = None,early_leave_minute:int=None,remark:str=None,**kwargs):
+def add_attend_cod(conn:pymysql.Connection ,date: str,employee_no:str,absent:int = None,shift_type:str = None,checkin_time:str=None,source:str=None,checkout_time:str=None,work_hours:str=None,location:str=None,approver_user_name:str=None,approved_at:str=None,late_minutes:int = None,early_leave_minutes:int=None,remark:str=None,**kwargs):
     try:
         approver_user_id = employee_id = None
         if employee_no:
@@ -28,9 +28,9 @@ def add_attend_cod(conn:pymysql.Connection ,date: str,employee_no:str,absent:int
             "checkout_time":checkout_time,
             "work_hours":work_hours,
             "location":location,
-            "approver_at":approver_at,
+            "approved_at":approved_at,
             "late_minutes":late_minutes,
-            "early_leave_minute":early_leave_minute,
+            "early_leave_minutes":early_leave_minutes,
             "remark":remark,
             "approver_user_id":approver_user_id
         }
@@ -56,7 +56,7 @@ def add_attend_cod(conn:pymysql.Connection ,date: str,employee_no:str,absent:int
     
 
 @regulate(0b1101)
-def update_attend_cod(conn:pymysql.Connection ,date: str,employee_no:str,absent:int = None,shift_type:str = None,checkin_time:str=None,source:str=None,checkout_time:str=None,work_hours:str=None,location:str=None,approver_user_name:str=None,approver_at:str=None,late_minutes:int = None,early_leave_minute:int=None,remark:str=None,status:str=None,**kwargs):
+def update_attend_cod(conn:pymysql.Connection ,date: str,employee_no:str,absent:int = None,shift_type:str = None,checkin_time:str=None,source:str=None,checkout_time:str=None,work_hours:str=None,location:str=None,approver_user_name:str=None,approved_at:str=None,late_minutes:int = None,early_leave_minutes:int=None,remark:str=None,status:str=None,**kwargs):
     try:
         approver_user_id = employee_id = None
         if employee_no:
@@ -78,9 +78,9 @@ def update_attend_cod(conn:pymysql.Connection ,date: str,employee_no:str,absent:
             "checkout_time":checkout_time,
             "work_hours":work_hours,
             "location":location,
-            "approver_at":approver_at,
+            "approved_at":approved_at,
             "late_minutes":late_minutes,
-            "early_leave_minute":early_leave_minute,
+            "early_leave_minutes":early_leave_minutes,
             "remark":remark,
             "approver_user_id":approver_user_id,
             "status":status
@@ -98,3 +98,86 @@ def update_attend_cod(conn:pymysql.Connection ,date: str,employee_no:str,absent:
     except Exception as e:
         print(f'❌ Unknow error from update_dept() : {e}')
         return False
+    
+@regulate(0b1101)
+def select_attend_cod(conn:pymysql.Connection ,status:str=None,date: str=None,employee_no:str=None,absent:int = None,shift_type:str = None,checkin_time:str=None,source:str=None,checkout_time:str=None,work_hours:str=None,location:str=None,approver_user_name:str=None,approved_at:str=None,late_minutes:int = None,early_leave_minutes:int=None,remark:str=None,**kwargs):
+    try:
+        approver_user_id = employee_id = None
+        if employee_no:
+            employee_data = om.mysql_select_dict(conn,"employee",{"employee_no":employee_no})["data"]
+            if employee_data:
+                employee_id = employee_data[0][0]
+            else:
+                print("❌ didn't have this employee")
+                return False
+        if approver_user_name!=None:
+            approver_user_id = om.mysql_select_dict(conn,"sys_user",{"username":approver_user_name})['data'][0][0]
+        
+        field_mapping = {
+            "date":date,
+            "absent":absent,
+            "attendance_record.employee_id":employee_id,
+            "shift_type":shift_type,
+            "checkin_time":checkin_time,
+            "source":source,
+            "checkout_time":checkout_time,
+            "work_hours":work_hours,
+            "location":location,
+            "approved_at":approved_at,
+            "late_minutes":late_minutes,
+            "early_leave_minutes":early_leave_minutes,
+            "remark":remark,
+            "approver_user_id":approver_user_id,
+            "attendance_record.status":status
+        }
+        select_data = {
+            **{k: v for k, v in field_mapping.items() if v is not None}
+        }
+        empl_contract_data = om.mysql_select_dict(conn,
+            tables=['attendance_record'],  # 主表使用别名
+            join_conditions=[
+                {
+                    'type': 'LEFT',
+                    'table1': 'attendance_record',
+                    'table2': 'employee',
+                    'on': 'attendance_record.employee_id = employee.employee_id'
+                },
+                {
+                    'type': 'LEFT',
+                    'table1': 'attendance_record',
+                    'table2': 'sys_user',
+                    'on': 'attendance_record.approver_user_id = sys_user.user_id'
+                }
+            ],
+            where_arg=select_data,
+            select_columns=[
+                "date",
+                "absent",
+                "shift_type",
+                "checkin_time",
+                "source",
+                "checkout_time",
+                "work_hours",
+                "location",
+                "approved_at",
+                "late_minutes",
+                "early_leave_minutes",
+                "remark",
+                "attendance_record.status",
+                'employee.name_cn',
+                "username"
+            ]
+        )
+        if empl_contract_data:
+            if empl_contract_data['data']:
+                return empl_contract_data
+            else:
+                print("❌ didn't select data form select_empl_doc()")
+                return empl_contract_data
+        else:
+            empl_contract_data = {'column_name':["error"],"data":["❌ mysql_select_dict error form select_empl_doc()"]}
+            return empl_contract_data
+
+    except Exception as e:
+        print(f'❌ Unknow error from select_select_empl_doccont() : {e}')
+        return {"column_name": ["error"],"data": [["Maybe Department name duplication from select_empl_doc()"]]}
